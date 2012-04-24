b69041149084efcd0f95202cd1d1e4dabeb78bef
Pablo Nussembaum <baunax@gmail.com>
2012-04-24 00:12:13 -0300

    new routes for tags db

--- src/exquisite_cadaver/routes.clj
+++ src/exquisite_cadaver/routes.clj
@@ -1,12 +1,28 @@
 (ns exquisite_cadaver.routes
   (:use compojure.core
-        exquisite_cadaver.views
+        exquisite_cadaver.views 
+        [exquisite_cadaver.db :as db] 
+        [clj-json.core :as json.db]
         [hiccup.middleware :only (wrap-base-url)])
   (:require [compojure.route :as route]
             [compojure.handler :as handler]
+            [clj-json.core :as json]
             [compojure.response :as response]))
 
+(defn json-response [data & [status]]
+  {:status (or status 200)
+   :headers {"Content-Type" "application/json"}
+   :body (json/generate-string data)})
+
+(db/addNew {:one 1})
+(db/addNew {:two 2})
+
 (defroutes main-routes
+  (GET "/tag" [] (json-response (db/find-all)))
+  (GET "/tag/:id" [id] (json-response (db/find id)))
+  (POST "/tag" {params :params}  (json-response (db/addNew params)))
+  (PUT "/tag/:id" {params :params} (json-response (db/update (params "id")  params)))
+  (DELETE "/tag/:id" [id] (json-response (db/delete id )))
   (GET "/" [] (index-page))
   (route/resources "/")
   (route/not-found "Page not found"))
