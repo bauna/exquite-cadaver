295d17e56b0d13bf9960ee89d9de10fadbc8bf16
Pablo Nussembaum <baunax@gmail.com>
2012-04-24 00:11:05 -0300

    new deps

--- project.clj
+++ project.clj
@@ -2,7 +2,10 @@
 	:description "Exquisite Cadaver @ Twitter"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [compojure "1.0.0"]
-                 [hiccup "0.3.7"]]
+                 [hiccup "0.3.7"]
+                 [clj-json "0.5.0"]
+                 [ring-json-params "0.1.3"]
+                 [clojure-twitter "1.2.6-SNAPSHOT"]]
   :dev-dependencies [[lein-ring "0.5.0"]
                      [lein-eclipse "1.0.0"]]
   :ring {:handler exquisite_cadaver.routes/app})
